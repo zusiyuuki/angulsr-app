4c18d8f726977ac35b140901804bd708e8d9fcb2
yuuki <zusiyuuki@gmail.com>
2021-10-08 09:36:16 +0900

    angularフロントエンド開発v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
